14e42f711e606c8426b8ae46c15619729f6e413e
hasanshahid99 <shahidhasan148@gmail.com>
2023-09-26 14:02:05 +0100

    navbar styled and icons added

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,20 +1,33 @@
 import './App.css'
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, {useState} from 'react'
 //import * as Pages from './pages';
 import { Routes, Route} from 'react-router-dom';
 import  Navbar from './components/Navbar'
 import HomePage from './pages/HomePage';
 import Discover from './pages/Discover';
+import NavItem from './components/Navbar/NavItem';
+import DropdownMenu from './components/Navbar/DropdownMenu';
+
 function App() {
  return (
-  <Routes>
-      {/*public routes */}
-      <Route path="/" element={<Navbar />}>
-      <Route index element={<HomePage />}/>
-      <Route path="/discover" element={<Discover/>}/>
-        </Route>
-</Routes>
+  <Navbar>
+    <NavItem icon="🌎"/>
+  <NavItem icon="💬"/>
+  <NavItem icon="🔔"/>
+  <NavItem icon="👤">
+    {/*dropdown menu*/}
+    <DropdownMenu />
+
+    </NavItem>
+    </Navbar> 
+//   <Routes>
+//       {/*public routes */}
+//       <Route path="/" element={<Navbar> <li>X</li> </Navbar>}>
+//       <Route index element={<HomePage />}/>
+//       <Route path="/discover" element={<Discover/>}/>
+//         </Route>
+// </Routes>
   )
 }
 
--- front-end/src/components/Navbar/index.jsx
+++ front-end/src/components/Navbar/index.jsx
@@ -1,56 +1,12 @@
-/* eslint-disable no-unused-vars */
-import React, {useState} from 'react'
-import './nav.css'
-import { NavLink, Outlet } from 'react-router-dom'
-
-const Navbar = () => {
-  const [isDropdownVisible, setDropdownVisible] = useState(false);
-
-  const toggleDropdown = () => {
-    setDropdownVisible(!isDropdownVisible);
-    console.log(isDropdownVisible)
-  };
- const styles = ({ isActive }) => ({
-  color: isActive ? 'black' : 'inherit', // Change the color for active elements
-  fontWeight: isActive ? 'bold' : 'normal',
-});
-
+import React from 'react'
+const Navbar = (props) => {
   return (
-    <>
-      <header className="navbar">
-      <nav className='nav-links'>
-        <NavLink to="/" className="logo">
-          Culturify
-        </NavLink>
+    <nav className='navbar'>
+      <ul className='navbar-nav'>{props.children}
+      </ul>
       
-        <NavLink to="/" className="link"style={styles} role="link">
-          HOME
-        </NavLink>
-        <NavLink to="/discover"className="link" style={styles}role="link">
-          DISCOVER
-        </NavLink>
-        <div className='user-icon' >
-        <button className='account-btn' onClick={toggleDropdown} style={{ backgroundImage: 'url("/src/assets/account-icon.png")', backgroundSize: 'cover' }}>
-  {/* Your content */}
-</button>
-
-     
-        </div>
-        {isDropdownVisible && (
-
-                <ul className="user-dropdown">
-                <li><a href="#">Profile</a></li>
-                <li><a href="#">Settings</a></li>
-                <li><a href="#">Log Out</a></li>
-                </ul>
-
-        )}
-      
-        </nav>
-
-      </header>
-      <Outlet />
-    </> )
+    </nav>
+  )
 }
 
 export default Navbar
