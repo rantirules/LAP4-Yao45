c30e1f5f3ecc506eb143e356add3749b89a2d591
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-09-26 12:25:29 +0100

    remove utils

--- front-end/src/components/Utils/getRandomColor.js
+++
@@ -1,9 +0,0 @@
-export default function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    console.log('GET RANDOM COLOR' + color)
-    return color;
-}
