960de6335dae3c14990bf5dd13360f5c6497f25d
hasanshahid99 <shahidhasan148@gmail.com>
2023-09-27 10:47:34 +0100

    there was some errors with the previous merges but all good now

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,14 +1,11 @@
 import React,{ useState, createContext } from 'react'
 import './App.css'
 // eslint-disable-next-line no-unused-vars
-import React, {useState} from 'react'
 //import * as Pages from './pages';
 import { Routes, Route} from 'react-router-dom';
 
 
 import  {Navbar, Chat} from './components/Navbar'
-import HomePage from './pages/HomePage';
-import Discover from './pages/Discover';
 import NavItem from './components/Navbar/NavItem';
 import DropdownMenu from './components/Navbar/DropdownMenu';
 import {HomePage, RegisterPage, Discover} from './pages';
--- front-end/src/components/Navbar/index.jsx
+++ front-end/src/components/Navbar/index.jsx
@@ -15,7 +15,6 @@ import { NavLink, Link,  Outlet } from 'react-router-dom'
 const Navbar = (props) => {
   
   return (
-
     <>
     <nav className='navbar'>
       <ul className='navbar-links'>
@@ -23,57 +22,20 @@ const Navbar = (props) => {
         <Link to="/">HOME</Link></li>
        <li>
         <Link to="/discover">DISCOVER</Link></li>
-     
-        </div>
-        {isDropdownVisible && (
 
-                <ul className="user-dropdown">
-                <li><a href="#">Profile</a></li>
-                <li><a href="#">Settings</a></li>
-                <li><a href="#">Log Out</a></li>
-                </ul>
+      </ul> 
 
-        )}
+      <ul className='navbar-nav'>{props.children}
+      </ul>
       
-        </nav>
 
-      <Outlet />
-    </> )
+    </nav>
 
-//     <div>
-//       <nav className="navbar">
-//       <div className="navbar-container">
-//         <Link to="/" className="navbar-logo">
-//           Your Logo
-//         </Link>
-//         <ul className="navbar-menu">
-//           <li className="navbar-item">
-//             <Link to="/home" className="navbar-link">
-//               Home
-//             </Link>
-//           </li>
-//           <li className="navbar-item">
-//             <Link to="/about" className="navbar-link">
-//               About
-//             </Link>
-//           </li>
-//           <li className="navbar-item">
-//             <Link to="/contact" className="navbar-link">
-//               Contact
-//             </Link>
-//           </li>
-//           <li className="navbar-item">
-//             <Link to="/register" className="navbar-link">
-//               Register
-//             </Link>
-//           </li>
-//           {/* Add more menu items as needed */}
-//         </ul>
-//       </div>
-//     </nav>
-//     <Outlet/>
-//     </div>
- // )
+     <Outlet/>
+     </>
+  )
+
+
 
 }
 
