aa2bbd6ac8e44532f47d1f5349b3115afedc248c
hasanshahid99 <shahidhasan148@gmail.com>
2023-09-28 16:59:28 +0100

    searchbar filters, filter function to change state of search via the filters.. now it's just connecting to the database to actually search our app

--- front-end/src/components/SearchFilters/SearchFilters.jsx
+++ front-end/src/components/SearchFilters/SearchFilters.jsx
@@ -2,29 +2,32 @@ import React, {useState} from 'react'
 import './filters.css';
 const SearchFilters = () => {
     const [showDropdown, setShowDropdown] = useState(null); // State to track active filter
+    const [checkedFilters, setCheckedFilters] = useState([]);
 
-    const filterDropdowns = {
-        'Region': [
-          'British ',
-          'Scandinavian',
-          'Mediterranean',
-          'South Asian'
-          // Add more options as needed for Region
-        ],
-        'Cooking Time': [
-          '5-10 mins',
-          '~20 mins',
-          '+30 mins'
-          // Add more options as needed for Cooking Time
-        ],
-        'No. of ppl': [
-            'Myself',
-            '2',
-            '3-5',
-            '7+'
-        ]
-      };
+    const handleCheckboxChange = (event,filterName) => {
+            if (event.target.checked) {
+                // If the checkbox is checked, add the filter to the checkedFilters state
+                setCheckedFilters([...checkedFilters, filterName]);
+                console.log("checked", filterName)
+              } else {
+                // If the checkbox is unchecked, remove the filter from the checkedFilters state
+                setCheckedFilters(checkedFilters.filter((filter) => filter !== filterName));
+                console.log("unchecked", filterName)
+              }
+            }
+            const displayCheckedFilters = () => {
+                if (checkedFilters.length === 0) {
+                  return ''; // Return an empty string if no filters are checked
+                }
+                // Join the checked filters using a comma and space
+                return `showing results for: ${checkedFilters.join(', ')}`,
+                console.log(checkedFilters);
+                
+              
+              };
+        
   return (
+    <>
     <div className="filter-bar">
     <ul>
       <li onMouseEnter={() => setShowDropdown('Region')}
@@ -36,10 +39,12 @@ const SearchFilters = () => {
             <div className="filter-menu">
               {/* Options for Region */}
               <label>
-                <input type="checkbox" /> British
+                <input type="checkbox" onChange={(event) => handleCheckboxChange(event, 'British')}
+                    checked={checkedFilters.includes('British')}/> <p className='check-item'>British</p>
               </label>
               <label>
-                <input type="checkbox" /> Scandinavian
+                <input type="checkbox"  onChange={(event) => handleCheckboxChange(event, 'Scandinavian')}
+                    checked={checkedFilters.includes('Scandinavian')} /> <p className='check-item'>Scandinavian</p>
               </label>
               {/* Add more options as needed for Region */}
             </div>
@@ -54,10 +59,12 @@ const SearchFilters = () => {
             <div className="filter-menu">
               {/* Options for Region */}
               <label>
-                <input type="checkbox" /> 5 mins
+                <input type="checkbox"  onChange={(event) => handleCheckboxChange(event, '5 mins')}
+                    checked={checkedFilters.includes('5 mins')}/> <p className='check-item'>5 mins</p>
               </label>
               <label>
-                <input type="checkbox" /> 10 mins
+                <input type="checkbox"  onChange={(event) => handleCheckboxChange(event, '10 mins')}
+                    checked={checkedFilters.includes('10 mins')}/> <p className='check-item'>10 mins</p>
               </label>
               </div>
          )}
@@ -65,25 +72,32 @@ const SearchFilters = () => {
       <li onMouseEnter={() => setShowDropdown('People')}
       onMouseLeave={()=> setShowDropdown(null)}>
         <label className='scaffold'>
-            No. of ppl
+            <p className='check-item'>No. of ppl</p>
             </label>
             {showDropdown === 'People' && (
             <div className="filter-menu">
               {/* Options for Region */}
               <label>
-                <input type="checkbox" /> Myself
+                <input type="checkbox"  onChange={(event) => handleCheckboxChange(event, 'Myself')}
+                    checked={checkedFilters.includes('Myself')}/> <p className='check-item'>Myself</p>
               </label>
               <label>
-                <input type="checkbox" /> 2 ppl
+                <input className="checkbox-pull"type="checkbox" onChange={(event) => handleCheckboxChange(event, '2 ppl')}
+                    checked={checkedFilters.includes('2 ppl')} /> <p className='check-item'>2 ppl</p>
               </label>
               </div>
          )}
       </li>
 
-      {/* Add more filters as needed */}
-    </ul>
+    </ul>   
+    
   </div>
+  <div className='display-search-filters'>
+  <p className='checked-filters'> Displaying search results for  {checkedFilters.join(', ')} </p>
+  </div>
+  </>
 );
 };
+
 
 export default SearchFilters
