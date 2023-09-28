a9e3ff5f03830e153b52f4c5f47c2e80a1c53a5b
SamM71 <sammocock@gmail.com>
2023-09-28 16:01:05 +0100

    display message when saving recipe

--- front-end/src/components/SaveRecipeButton/index.jsx
+++ front-end/src/components/SaveRecipeButton/index.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 const SaveRecipeButton = (props) => {
+  const [message, setMessage] = useState('')
   const url = `http://127.0.0.1:5000/saved/${props.uid}/${props.rid}`
 
   async function saveRecipe() {
     const res = await axios.post(url)
     if (res.data.error) {
       console.log("Recipe already saved")
+      setMessage('Recipe is already saved.')
     } else if (res.status === 201) {
-      console.log("Recipe saved")
+      console.log("Recipe saved.")
+      setMessage("Recipe saved.")
     }
+    setTimeout(() => {
+      setMessage('')
+    }, 3000)
+    // timeout will disappear after 3 seconds even if pressed again
   }
 
   return (
-    <button onClick={saveRecipe}>Save Recipe</button>
+    <>
+      <button onClick={saveRecipe}>Save Recipe</button>
+      <p>{message}</p>
+    </>
   )
 }
 
