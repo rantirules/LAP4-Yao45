d318974876f02d29379fd15c337c5110c5893bad
SamM71 <sammocock@gmail.com>
2023-09-28 14:55:14 +0100

    refactor recipe page and component

--- front-end/src/components/Recipe/index.jsx
+++ front-end/src/components/Recipe/index.jsx
@@ -1,43 +1,39 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Recipe = () => {
+const Recipe = (props) => {
   const [loading, setLoading] = useState(false)
-  const [recipe, setRecipe] = useState({})
   const [creator, setCreator] = useState('')
 
   useEffect(() => {
-    async function loadRecipe() {
-      const res = await axios.get("http://127.0.0.1:5000/recipes/1")
-      const recipe = await res.data.recipe
-      console.log(recipe)
-      setRecipe(recipe)
-      setLoading(false)
-
+    setLoading(true)
+    async function loadUser() {
       // get username
-      const id = await recipe.user_id
-      const resp = await axios.get(`http://127.0.0.1:5000/users/${id}`)
-      setCreator(resp.data.user.username)
+      if (props.user_id) {
+        const id = await props.user_id
+        const resp = await axios.get(`http://127.0.0.1:5000/users/${id}`)
+        setCreator(resp.data.user.username)
+        setLoading(false)
+      }
     }
-
-    loadRecipe()
-  }, [])
+    loadUser()
+  }, [props.user_id])
 
   function displayRecipe() {
     return (
       <main>
-        <h1>{recipe.name}</h1>
+        <h1>{props.name}</h1>
         <div>
           Created by: {creator}
         </div>
         <div>
-          Culture: {recipe.culture}
+          Culture: {props.culture}
         </div>
         <div>
-          Description: {recipe.description}
+          Description: {props.desc}
         </div>
         <div>
-          Image URL: {recipe.img_url}
+          Image URL: {props.img}
         </div>
       </main>
     )
--- front-end/src/pages/RecipePage/index.jsx
+++ front-end/src/pages/RecipePage/index.jsx
@@ -2,11 +2,25 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Recipe, SaveRecipeButton } from '../../components'
 
-const RecipePage = () => {
+const RecipePage = ({ user_id }) => {
+  const [recipe, setRecipe] = useState({})
+  const recipe_id = 1 // remove this once id is passed
+
+  useEffect(() => {
+    async function loadRecipe() {
+      const res = await axios.get(`http://127.0.0.1:5000/recipes/${recipe_id}`)
+      const recipe = await res.data.recipe
+      // console.log(recipe)
+      setRecipe(recipe)
+    }
+    loadRecipe()
+  }, [])
   return (
+      // both components must know the recipe id
+      // the button must also know the user_id
     <>
-      <Recipe />
-      <SaveRecipeButton />
+      <Recipe rid={recipe.id} name={recipe.name} culture={recipe.culture} desc={recipe.description} img={recipe.img_url} user_id={recipe.user_id} />
+      <SaveRecipeButton rid={recipe.id} user_id={user_id} />
     </>
 
   )
