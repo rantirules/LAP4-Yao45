2cc79b1cd6438621ebe8b2f34df3925d60d79dec
“rkumari2” <rubinakumari@mail.com>
2023-10-02 11:25:56 +0100

    home page title styling

--- front-end/src/pages/HomePage/index.jsx
+++ front-end/src/pages/HomePage/index.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable no-unused-vars */
 
 import React from 'react'
+import './home.css'
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to Homepage</h1>
+    <div id='home-page'>
+      <div id='title'>
+        <h1>Explore, Savour, Connect: <span> Culturify </span> Your World!</h1>
+      </div>
     </div>
   )
 }
