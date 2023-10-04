79bd7b08d18539544895ca09f4186862d634b14d
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-10-04 19:02:24 +0100

    revert chat to before

--- front-end/src/components/Chat/index.jsx
+++ front-end/src/components/Chat/index.jsx
@@ -23,47 +23,72 @@ const Chat = () => {
     const [users, setUsers] = useState([])
     const [dialogues, setDialogues] = useState([])
     const [currentDialogue, setCurrentDialogue] = useState()
-    const displayName = userName
+    const [user, setUser] = useState('')
+    const currentUserId = userName
+    const [loading, setLoading] = useState(false)
 
 
 
     useEffect(() => {
+      console.log(currentUserId);
+      const getCurrentUser = async () => {
+        const response = await fetch(`http://127.0.0.1:5000/users/${currentUserId}`)
+        const data = await response.json()
+        setUser(data.user.username)
+      }
       const getUsers = async () => {
         const response = await fetch('http://127.0.0.1:5000/users')
         const data = await response.json()
         setUsers(data.users)
       }
       const getDialogues = async () => {
-        const response = await fetch(`http://127.0.0.1:5000/dialogues/${displayName}`)
+        const response = await fetch(`http://127.0.0.1:5000/dialogues`)
         const data = await response.json()
         setDialogues(data.dialogues)
-        console.log(data.dialogues);
+        console.log(dialogues);
       }
+      getCurrentUser()
       getUsers()
       getDialogues()
     },[])
 
     const createNewChat = async () => {
+      const getReceiver = await fetch('http://127.0.0.1:5000/users')
+      const receiverData = await getReceiver.json()
+      const receiver = receiverData.users[Math.floor(Math.random() * receiverData.users.length)].username
+      console.log(receiver)
+      const dialogue_arr = await fetch('http://127.0.0.1:5000/dialogues')
+      const arr_data = await dialogue_arr.json()
+      console.log(arr_data);
+      const dialogue_id = arr_data.dialogues.length + 1
+      if (!user){
+        console.log('USER NOT DEFINED')
+        return
+      }
+      const newDialogue = { user, receiver, dialogue_id}
       const options = {method: "POST", headers: {
         'Content-Type' : 'application/json'
         },
-        body: JSON.stringify(displayName)
+        body: JSON.stringify(newDialogue)
     }
-      const response = await fetch('http://127.0.0.1:5000/dialogues', options)
+      const create = await fetch('http://127.0.0.1:5000/dialogues', options)
+      const refreshDialogues = await fetch(`http://127.0.0.1:5000/dialogues/${user}`)
+      const data = await refreshDialogues.json()
+      console.log(dialogues);
     }
-
+    console.log(dialogues)
   return (
     <div className='chat-container'>
       <div className='sidebar'>
         <div className='navbar-chat'>
           <h3 className='chat-title'>Culturify Chat</h3>
           <div className='user-log'>
-            <p>{displayName}</p>
+            <p>{user}</p>
           </div>
         </div>
         <div className='chats'>
           <h4 className='user-title'>Users:</h4>
-          {dialogues && dialogues.map((u, idx) => 
+          {dialogues.map((u, idx) => 
             <ProfileIcon username={u.username} receiver={u.receiver} key={idx} dialogue_id={u.id} currentDialogue={currentDialogue} setCurrentDialogue={setCurrentDialogue} setMessages={setMessages} messages={messages}/>
           )}  
         </div>
@@ -78,7 +103,7 @@ const Chat = () => {
       </div>
       {dialogues ? 
       <div className='message-container'>
-          <Messages messages={messages} setMessages={setMessages} formValue={formValue} setFormValue={setFormValue} currentDialogue={currentDialogue}/>
+          <Messages messages={messages} setMessages={setMessages} formValue={formValue} setFormValue={setFormValue} currentDialogue={currentDialogue} currentUser={user}/>
       </div> : <div>No chat loaded</div>}
     </div>
   )
